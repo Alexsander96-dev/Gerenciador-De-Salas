54e81e398674063eb691429bf646df180b7f3f8d
Alexsander Santos <santosalexsander151@gmail.com>
2026-08-13 16:32:19 -0300

    iniciado projeto

---
+++ src/test/java/br/com/alexsander/reservas_de_salas/ReservasDeSalasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alexsander.reservas_de_salas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservasDeSalasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
