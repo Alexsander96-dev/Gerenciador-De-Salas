1fe6c1184d9d0a22e7eaee27a86478a5bca46885
Alexsander Santos <santosalexsander151@gmail.com>
2026-08-14 12:21:19 -0300

    adicionado controller das entidades

---
+++ src/main/java/br/com/alexsander/reservas_de_salas/controller/ReservaController.java
@@ -0,0 +1,9 @@
+package br.com.alexsander.reservas_de_salas.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/reservas")
+public class ReservaController {
+}
